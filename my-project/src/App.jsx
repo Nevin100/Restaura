4f2ad65d14d504bf3af4191ecde654981490c708
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-05 19:03:27 +0530

    My-Restaraunt website folder

---
+++ my-project/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Hero from "./components/Hero.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Dishes from "./components/Dishes.jsx";
+import About from "./components/About.jsx";
+import Mission from "./components/Mission.jsx";
+import Expertise from "./components/Expertise.jsx";
+import Reviews from "./components/Reviews.jsx";
+
+const App = () => {
+  return (
+    <div className="overflow-y-hidden text-neutral-200 antialiased">
+      <Hero />
+      <Navbar />
+      <Dishes />
+      <About />
+      <Mission />
+      <Expertise />
+      <Reviews />
+    </div>
+  );
+};
+
+export default App;
---
+++ my-project/src/components/Reviews.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { REVIEW } from "../constants";
+import xaviour from "../assets/xaviour.jpeg";
+import customer1 from "../assets/customer1.jpeg";
+import customer2 from "../assets/customer2.jpeg";
+import customer3 from "../assets/customer3.jpeg";
+import customer4 from "../assets/customer4.jpeg";
+
+const Reviews = () => {
+  return (
+    <section className="container mx-auto mb-8 mt-12" id="review">
+      <div className="flex flex-col">
+        <p className="mb-10 text-3xl font-light leading-normal tracking-tighter lg:mx-40 lg:mt-40 lg:text-[3.5rem]">
+          {REVIEW.content}
+        </p>
+        <div className="flex items-center justify-center gap-6">
+          |
+          <img
+            src={xaviour}
+            width={80}
+            height={80}
+            alt={REVIEW.name}
+            className="rounded-full border"
+          />
+          <div className="tracking-tighter">
+            <h6 className="">{REVIEW.name}</h6>
+            <p className=""> </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Reviews;
