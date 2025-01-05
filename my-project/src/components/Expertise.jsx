d7572ddb515f921a01fb9bfcbb07f0988933f82a
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-05 21:50:28 +0530

    motion update in Expertise

--- my-project/src/components/Expertise.jsx
+++ my-project/src/components/Expertise.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { CUSINES } from "../constants";
+import { motion } from "framer-motion";
 
 const Expertise = () => {
   return (
     <section id="expertise">
-      <h2 className="my-8 text-center text-3xl tracking-tighter lg:text-4xl">
+      <motion.h2
+        nitial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 0.2 }}
+        className="my-8 text-center text-3xl tracking-tighter lg:text-4xl"
+      >
         Our Expertise
-      </h2>
+      </motion.h2>
       <div className="container mx-auto px-4">
         {CUSINES.map((cusine, index) => (
           <div
@@ -14,17 +20,27 @@ const Expertise = () => {
             key={index}
           >
             <div className="flex-shrink-0 pr-8 text-2xl">{cusine.number}</div>
-            <div className="w-1/3 flex-shrink-0">
+            <motion.div
+              initial={{ opacity: 0, x: -50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 4, delay: 1.2 }}
+              className="w-1/3 flex-shrink-0"
+            >
               <img src={cusine.image} className="h-auto rounded-3xl" />
-            </div>
-            <div className="pl-8">
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: 50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 4, delay: 1.2 }}
+              className="pl-8"
+            >
               <h3 className="text-2xl uppercase tracking-tighter text-rose-300">
                 {cusine.title}
               </h3>
               <p className="mt-4 text-lg tracking-tighter">
                 {cusine.description}
               </p>
-            </div>
+            </motion.div>
           </div>
         ))}
       </div>
