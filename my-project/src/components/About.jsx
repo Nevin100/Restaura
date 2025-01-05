2ee2068429b46470a04d9c2abd35055aebc4ef3d
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-05 21:41:40 +0530

    motion update in About

--- my-project/src/components/About.jsx
+++ my-project/src/components/About.jsx
@@ -1,25 +1,44 @@
 import React from "react";
 import about from "../assets/about.jpeg";
 import { ABOUT } from "../constants";
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
     <section className="container mx-auto mb-8" id="about">
-      <h2 className="mb-8 text-center text-3xl tracking-tighter lg:text-4xl">
+      <motion.h2
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1, delay: 0.2 }}
+        className="mb-8 text-center text-3xl tracking-tighter lg:text-4xl"
+      >
         About Us
-      </h2>
+      </motion.h2>
       <div className="flex flex-wrap">
-        <div className="w-full p-4 lg:w-1/2">
+        <motion.div
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 4, delay: 1.2 }}
+          className="w-full p-4 lg:w-1/2"
+        >
           <img src={about} className="rounded-3xl lg:-rotate-3" />
-        </div>
+        </motion.div>
         <div className="w-full px-2 lg:w-1/2">
-          <h2 className="text-4xl tracking-tighter lg:text-6xl">
+          <motion.h2
+            initial={{ opacity: 0, x: 50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 4, delay: 1.2 }}
+            className="text-4xl tracking-tighter lg:text-6xl"
+          >
             {ABOUT.header}
             <div className="mb-8 mt-1 h-2 w-36 bg-rose-300 lg:-rotate-3"></div>
             <p className="m-8 text-2xl leading-relaxed tracking-tight lg:max-w-xl">
               {ABOUT.content}
             </p>
-          </h2>
+          </motion.h2>
         </div>
       </div>
     </section>
