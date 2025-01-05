cc99dca9b2a0271f0e776786bd85653a82df2f92
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-05 21:45:40 +0530

    motion update in Dishes

--- my-project/src/components/Dishes.jsx
+++ my-project/src/components/Dishes.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import { DISHES } from "../constants";
 import DishCard from "../components/DishCard.jsx";
+import { motion } from "framer-motion";
+
 const Dishes = () => {
   return (
     <section className="container mx-auto py-16" id="dishes">
-      <h2 className="mb-8 text-center text-3xl tracking-tighter lg:text-4xl">
+      <motion.h2
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 0.2 }}
+        className="mb-8 text-center text-3xl tracking-tighter lg:text-4xl"
+      >
         Our Dishes
-      </h2>
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-5">
+      </motion.h2>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 0.2 }}
+        className="grid grid-cols-1 gap-8 md:grid-cols-5"
+      >
         {DISHES.map((project, index) => (
           <DishCard key={index} project={project} />
         ))}
-      </div>
+      </motion.div>
     </section>
   );
 };
